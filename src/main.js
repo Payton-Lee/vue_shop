6e9eb3f2efdffa0de87cb5dfad53487828ce992e
宗介 <perusrejonas1202@163.com>
2022-01-22 20:49:14 +0800

    完成商品功能开发

--- src/main.js
+++ src/main.js
@@ -8,6 +8,12 @@ import './assets/css/global.css'
 import './assets/fonts/iconfont.css'
 // 
 import ZkTable from 'vue-table-with-tree-grid'
+// 引入富文本框编辑器
+import VueQuillEditor from 'vue-quill-editor'
+// d导入富文本编辑器的样式
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
 
 import axios from 'axios'
 // 配置請求根路徑
@@ -21,6 +27,23 @@ Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 Vue.component('tree-table', ZkTable)
+// 将富文本编辑器注册为全局可用的组件
+Vue.use(VueQuillEditor)
+Vue.filter('dateFormat', function(originVal){
+  const dt = new Date(originVal)
+  const y = dt.getFullYear()
+  const m = (dt.getMonth() + 1 + '').padStart(2, '0')
+  // padStart：padStart() 方法用另一个字符串填充当前字符串(如果需要的话，会重复多次)，以便产生的字符串达到给定的长度。从当前字符串的左侧开始填充。
+  // 语法：str.padStart(targetLength [, padString])
+  // targetLength： 当前字符串需要填充到的目标长度。如果这个数值小于当前字符串的长度，则返回当前字符串本身。
+  // padString 可选：填充字符串。如果字符串太长，使填充后的字符串长度超过了目标长度，则只保留最左侧的部分，其他部分会被截断。此参数的默认值为 " "（U+0020）。
+  // 返回值：在原字符串开头填充指定的填充字符串直到目标长度所形成的新字符串。
+  const d = (dt.getDate() + '').padStart(2, '0')
+  const hh = (dt.getHours() + '').padStart(2, '0')
+  const mm = (dt.getMinutes() + '').padStart(2, '0')
+  const ss = (dt.getSeconds() + '').padStart(2, '0')
+  return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+})
 
 new Vue({
   router,
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -37,7 +37,12 @@ import {
   Cascader,
   Alert,
   Tabs,
-  TabPane
+  TabPane,
+  Step,
+  Steps,
+  Checkbox,
+  CheckboxGroup,
+  Upload
 } from 'element-ui'
 
 Vue.use(Button)
@@ -70,6 +75,11 @@ Vue.use(Cascader)
 Vue.use(Alert)
 Vue.use(Tabs)
 Vue.use(TabPane)
+Vue.use(Steps)
+Vue.use(Step)
+Vue.use(CheckboxGroup)
+Vue.use(Checkbox)
+Vue.use(Upload)
 Vue.prototype.$message = Message
 // 原型链挂载
 Vue.prototype.$confirm = MessageBox.confirm
